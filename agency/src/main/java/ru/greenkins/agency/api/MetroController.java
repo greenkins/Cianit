f771a12dd4cfeb8c7ea347d09e583bf7e3aa6cc8
greenkins <grebenkinvd@gmail.com>
2025-02-09 02:10:17 +0300

    [+] Agency: all api methods released. (MVP)

--- agency/src/main/java/ru/greenkins/agency/api/MetroController.java
+++ agency/src/main/java/ru/greenkins/agency/api/MetroController.java
@@ -1,22 +1,75 @@
 package ru.greenkins.agency.api;
 
 import jakarta.ws.rs.*;
-import jakarta.ws.rs.core.Context;
-import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.core.UriInfo;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.*;
 import lombok.Getter;
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import ru.greenkins.agency.api.errors.BadRequestExceptionMapper;
 import ru.greenkins.agency.api.errors.ServerErrorExceptionMapper;
+
+import java.net.URI;
 
 @Getter
 @Path("/get-ordered-by-time-to-metro")
 public class MetroController {
     @Context
     private UriInfo uriInfo;
+    Config config = ConfigProvider.getConfig();
+    private final String HOUSING_SERVICE_URL = config.getValue("housing.service.url", String.class); // URL из конфига
+
+    @GET
+    @Path("/")
+    @Produces("application/xml")
+    public Response wrongWay(){
+        return BadRequestExceptionMapper.getResponse(400, "Неверный запрос", uriInfo.getPath());
+    }
+
+    @GET
+    @Path("/{by-transport}")
+    @Produces("application/xml")
+    public Response wrongWay2(){
+        return BadRequestExceptionMapper.getResponse(400, "Неверный запрос", uriInfo.getPath());
+    }
 
     @GET
     @Path("/{by-transport}/{desc}")
     @Produces("application/xml")
     public Response getMetro(@PathParam("by-transport") boolean byTransport, @PathParam("desc") boolean desc) {
-        return ServerErrorExceptionMapper.getResponse("Не готово!", uriInfo.getPath());
+        System.out.println("Запрос на получение отсортированного списка квартир: " + uriInfo.getBaseUri());
+        try {
+            Client client = ClientBuilder.newClient(); // Создаём JAX-RS клиент
+            // Формируем базовый URL для первого сервиса
+            URI baseUri = URI.create(HOUSING_SERVICE_URL);
+            WebTarget target;
+            if (byTransport)
+                target = client.target(UriBuilder.fromUri(baseUri).path("/flats")).queryParam("sort", (desc ? "-transport" : "transport")).queryParam("size", 1000);
+            else
+                target = client.target(UriBuilder.fromUri(baseUri).path("/flats")).queryParam("sort", (desc ? "-area" : "area")).queryParam("size", 1000);
+            // Выполняем GET-запрос
+            Response housingResponse = target.request(MediaType.APPLICATION_XML).get();
+            // Проверяем статус ответа
+            String responseEntity = housingResponse.readEntity(String.class);
+            if (responseEntity == null || responseEntity.isEmpty())
+                return ServerErrorExceptionMapper.getResponse(503, "Внутренняя ошибка сервера", uriInfo.getPath());
+
+            if (housingResponse.getStatus() == 200) {
+                client.close();
+                return Response.ok(responseEntity).build();
+            } else if (housingResponse.getStatus() == 404) {
+                return ServerErrorExceptionMapper.getResponse(503, "Внутренняя ошибка сервера", uriInfo.getPath());
+            } else {
+                return Response.status(housingResponse.getStatus()).entity("").build();
+            }
+        } catch (ProcessingException e) {
+            return ServerErrorExceptionMapper.getResponse(503, "Внутренняя ошибка сервера", uriInfo.getPath());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return ServerErrorExceptionMapper.getResponse("Внутренняя ошибка сервера", uriInfo.getPath());
+        }
     }
 }
---
+++ agency/src/main/java/ru/greenkins/agency/model/Flat.java
@@ -0,0 +1,71 @@
+package ru.greenkins.agency.model;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Positive;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import ru.greenkins.agency.util.LocalDateAdapter;
+
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@XmlRootElement(name = "Flat")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Flat implements Serializable {
+    public Flat(int id, @NotNull String name, @NotNull Coordinates coordinates, @Null LocalDate creationDate, double area, @NotNull Long numberOfRooms,
+                int livingSpace, @Null Boolean isNew, @Null Transport transport, @NotNull House house) {
+        this.id = id;
+        this.name = name;
+        this.coordinates = coordinates;
+        this.creationDate = creationDate == null ? LocalDate.now() : creationDate;
+        this.area = area;
+        this.numberOfRooms = numberOfRooms;
+        this.isNew = isNew;
+        this.transport = transport;
+        this.house = house;
+    }
+
+    public Flat(int id) {
+        this.id = id;
+        this.name = "testFlat" + id;
+        this.coordinates = new Coordinates(7, 7);
+        this.creationDate = LocalDate.now();
+        this.area = 0.0;
+        this.numberOfRooms = 1L;
+        this.isNew = true;
+        this.transport = null;
+        this.house = new House("TestEliteHouse", 2004, 5L, 2);
+    }
+
+    @XmlElement
+    private int id;                 //Значение поля должно быть больше 0, Значение этого поля должно быть уникальным, Значение этого поля должно генерироваться автоматически
+    @XmlElement @NotNull @XmlJavaTypeAdapter(LocalDateAdapter.class)
+    private LocalDate creationDate;   //Поле не может быть null, Значение этого поля должно генерироваться автоматически
+    @XmlElement @NotNull
+    private String name;            //Поле не может быть null, Строка не может быть пустой
+    @XmlElement @NotNull
+    private Coordinates coordinates;    //Поле не может быть null
+    @XmlElement @NotNull @Positive
+    private double area;            //Значение поля должно быть больше 0
+    @XmlElement @NotNull @Positive
+    private Long numberOfRooms;     //Поле не может быть null, Значение поля должно быть больше 0
+    @XmlElement @Positive
+    private int livingSpace;        //Значение поля должно быть больше 0
+    @XmlElement(name = "new") @Null
+    private Boolean isNew;          //Поле может быть null
+    @XmlElement @Null
+    private Transport transport;    //Поле может быть null
+    @XmlElement @NotNull
+    private House house;            //Поле не может быть null
+}
