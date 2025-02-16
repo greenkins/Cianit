c4bbb3d7be4723ff7ce350700e20ab604aba4d24
greenkins <grebenkinvd@gmail.com>
2025-02-16 19:43:14 +0300

    [+] Housing-M, Agency-M (rest, ejb) init

---
+++ housing-m/src/main/java/ru/greenkins/housingm/HousingMApplication.java
@@ -0,0 +1,13 @@
+package ru.greenkins.housingm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousingMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousingMApplication.class, args);
+	}
+
+}
